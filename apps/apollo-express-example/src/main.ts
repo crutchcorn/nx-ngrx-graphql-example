9719291c0f8df918ca90a944d1231253f07cbd27
Corbin Crutchley <crutchcorn@gmail.com>
2020-11-04 19:23:14 -0800

    Add initial Apollo server example

--- apps/apollo-express-example/src/main.ts
+++ apps/apollo-express-example/src/main.ts
@@ -4,15 +4,39 @@
  */
 
 import * as express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to apollo-express-example!' });
 });
 
 const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+
+const expressServer = app.listen(port, () => {
+  console.log(`REST Listening at http://localhost:${port}/api`);
+  console.log(
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+  );
 });
-server.on('error', console.error);
+
+expressServer.on('error', console.error);
