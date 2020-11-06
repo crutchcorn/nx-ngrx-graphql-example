9771a4fe30fa3fff71cd204d1efc703cff04c851
Corbin Crutchley <crutchcorn@gmail.com>
2020-11-05 17:37:56 -0800

    Remove additional console warn

--- apps/apollo-express-example/ormconfig.ts
+++ apps/apollo-express-example/ormconfig.ts
@@ -2,8 +2,6 @@ import { ConnectionOptions } from 'typeorm';
 import { resolve } from 'path';
 import { Todos } from './src/models';
 
-console.warn(resolve(__dirname, './src/models/index.ts'));
-
 export const ORMConfig: ConnectionOptions = {
   type: 'sqlite',
   database: resolve(__dirname, './src/database/database.sqlite3'),
